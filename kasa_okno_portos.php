8b6921c32c6ae0e0f678b417fdbb3693575de36d
luboshs <lubomirantal@gmail.com>
2026-08-24 01:08:41 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- kasa_okno_portos.php
+++ kasa_okno_portos.php
@@ -1394,10 +1394,7 @@
                         echo '</tr>';
 
                         echo '<tr>';
-                        echo '<td>';
-                        echo '';
-                        echo '</td>';
-                        echo '<td>';
+                        echo '<td colspan="2">';   
                         echo '<br />Pre pokračovanie sa spýtaj klienta na spôsob platby a klikni nižšie:<br /><br />';
                         echo '</td>';
                         echo '<td>';
@@ -1406,10 +1403,7 @@
                         echo '</tr>';
 
                         echo '<tr>';
-                        echo '<td>';                                                                                   
-                        echo '';
-                        echo '</td>';
-                        echo '<td>';
+                        echo '<td colspan="2">';                                                                                                       
                         echo '<button type="button" onclick="location.hash = '."'#HotovostTR'".'; document.getElementById('."'hotovost'".').focus();" class="button_platba">IBA <br />HOTOVOS&#356;</button> &nbsp';
                         echo '<button type="button" onclick="document.getElementById('."'hotovost'".').focus(); location.hash = '."'#PlatbaKartou'".'; platbaKartou();" class="button_platba">PLATBA <br />KARTOU</button> &nbsp';
                         echo '<button type="button" onclick="location.hash = '."'#QrStatusTR'".'; qrPlatba();" class="button_platba">QR <br />PLATBA</button>';
